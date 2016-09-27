1200d5c5e2f893b0ad9667238f3892efd10a9cab
Johan Mabille <johan.mabille@gmail.com>
2016-09-27 23:31:07 +0000

    xarray_adaptor test

--- test/test_xarray_adaptor.cpp
+++ test/test_xarray_adaptor.cpp
@@ -7,7 +7,87 @@ namespace qs
     using vec_type = std::vector<int>;
     using adaptor_type = xarray_adaptor<vec_type>;
 
+    TEST(xarray_adaptor, shaped_constructor)
+    {
+        {
+            SCOPED_TRACE("row_major constructor");
+            row_major_result rm;
+            vec_type v;
+            adaptor_type a(v, rm.shape());
+            compare_shape(a, rm);
+        }
+
+        {
+            SCOPED_TRACE("column_major constructor");
+            column_major_result cm;
+            vec_type v;
+            adaptor_type a(v, cm.shape(), layout::column_major);
+            compare_shape(a, cm);
+        }
+    }
     
+    TEST(xarray_adaptor, strided_constructor)
+    {
+        central_major_result cmr;
+        vec_type v;
+        adaptor_type a(v, cmr.shape(), cmr.strides());
+        compare_shape(a, cmr);
+    }
+
+    TEST(xarray_adaptor, copy_semantic)
+    {
+        central_major_result res;
+        int value = 2;
+        vec_type v(res.size(), value);
+        adaptor_type a(v, res.shape(), res.strides());
+
+        {
+            SCOPED_TRACE("copy constructor");
+            adaptor_type b(a);
+            compare_shape(a, b);
+            EXPECT_EQ(a.data(), b.data());
+        }
+
+        {
+            SCOPED_TRACE("assignment operator");
+            row_major_result r;
+            vec_type v(r.size(), 0);
+            adaptor_type c(v, r.shape());
+            EXPECT_NE(a.data(), c.data());
+            c = a;
+            compare_shape(a, c);
+            EXPECT_EQ(a.data(), c.data());
+        }
+    }
+
+    TEST(xarray_adaptor, move_semantic)
+    {
+        central_major_result res;
+        int value = 2;
+        vec_type v(res.size(), value);
+        adaptor_type a(v, res.shape(), res.strides());
+
+        {
+            SCOPED_TRACE("move constructor");
+            adaptor_type tmp(a);
+            adaptor_type b(std::move(tmp));
+            compare_shape(a, b);
+            EXPECT_EQ(a.data(), b.data());
+        }
+
+        {
+            SCOPED_TRACE("move assignment");
+            row_major_result r;
+            vec_type v(r.size(), 0);
+            adaptor_type c(v, r.shape());
+            EXPECT_NE(a.data(), c.data());
+            adaptor_type tmp(a);
+            c = std::move(tmp);
+            compare_shape(a, c);
+            EXPECT_EQ(a.data(), c.data());
+        }
+    }
+
     TEST(xarray_adaptor, reshape)
     {
         vec_type v;
