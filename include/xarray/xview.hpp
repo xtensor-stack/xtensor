2db201f19e98d181719ff2d725f5242eac428ce5
Sylvain Corlay <sylvain.corlay@gmail.com>
2016-10-07 19:27:26 +0200

    Refactor xview

--- include/xarray/xview.hpp
+++ include/xarray/xview.hpp
@@ -15,7 +15,185 @@ namespace qs
 {
 
     /*********************************
-     * squeeze count
+     * xview declaration
+     *********************************/
+
+    template <class E, class... S>
+    class xview : public xexpression<xview<E, S...>>
+    {
+
+    public:
+
+        using self_type = xview<E, S...>;
+
+        using value_type = typename E::value_type;
+        using reference = typename E::reference;
+        using const_reference = typename E::const_reference;
+        using pointer = typename E::pointer;
+        using const_pointer = typename E::const_pointer;
+        using size_type = typename E::size_type;
+        using difference_type = typename E::difference_type;
+
+        using shape_type = xshape<size_type>;
+        using closure_type = const self_type&;
+
+        xview(E&& e, S&&... slices) noexcept;
+
+        size_type dimension() const noexcept;
+        
+        auto shape() const noexcept;
+
+        bool broadcast_shape(shape_type& shape) const;
+
+        template <class... Args>
+        reference operator()(Args... args);
+
+        template <class... Args>
+        const_reference operator()(Args... args) const;
+
+    private:
+
+        E& m_e;
+        std::tuple<S...> m_slices;
+        shape_type m_shape;
+
+        template <size_type... I, class... Args>
+        reference access_impl(std::index_sequence<I...>, Args... args);
+
+        template <size_type... I, class... Args>
+        const_reference access_impl(std::index_sequence<I...>, Args... args) const;
+
+        template <size_type I, class... Args>
+        std::enable_if_t<(I<sizeof...(S)), size_type> index(Args... args) const;
+
+        template <size_type I, class... Args>
+        std::enable_if_t<(I>=sizeof...(S)), size_type> index(Args... args) const;
+
+        template<size_type I, class T, class... Args>
+        size_type sliced_access(const xslice<T>& slice, Args... args) const;
+
+        template<size_type I, class T, class... Args>
+        disable_xslice<T, size_type> sliced_access(const T& squeeze, Args...) const;
+
+    };
+
+    template <class E, class... S>
+    inline xview<E, S...> make_xview(E& e, S&&... slices);
+
+    // number of integral types in the specified sequence of types
+    template <class... S>
+    inline constexpr size_t squeeze_count();
+
+    // number of integral types in the specified sequence of types before specified index.
+    template <class... S>
+    inline constexpr size_t squeeze_count_before(size_t i);
+
+    // index in the specified sequence of types of the ith non-integral type.
+    template <class... S>
+    inline constexpr size_t non_squeeze(size_t i);
+
+    /*********************************
+     * xview implementation
+     *********************************/
+
+    template <class E, class... S>
+    inline xview<E, S...>::xview(E&& e, S&&... slices) noexcept : m_e(e), m_slices(slices...)
+    {
+        constexpr size_t last_slice_index = sizeof...(S) - squeeze_count<S...>();
+        auto func = [](auto s) { return get_size(s); };
+        m_shape.resize(dimension());
+        for (size_type i = 0; i != last_slice_index; ++i)
+        {
+            m_shape[i] = apply<size_t>(non_squeeze<S...>(i), func, std::forward<S>(slices)...);
+        }
+        for (size_type i = last_slice_index; i != dimension(); ++i)
+        {
+            m_shape[i] = m_e.shape()[non_squeeze<S...>(i)];
+        }
+    }
+
+    template <class E, class... S>
+    inline auto xview<E, S...>::dimension() const noexcept -> size_type
+    {
+        return m_e.dimension() - squeeze_count<S...>();
+    }
+        
+    template <class E, class... S>
+    inline auto xview<E, S...>::shape() const noexcept
+    {
+        return m_shape;
+    }
+
+    template <class E, class... S>
+    inline auto xview<E, S...>::broadcast_shape(shape_type& shape) const -> bool
+    {
+        return false;
+    }
+
+    template <class E, class... S>
+    template <class... Args>
+    inline auto xview<E, S...>::operator()(Args... args) -> reference
+    {
+        return access_impl(std::make_index_sequence<sizeof...(Args) + squeeze_count<S...>()>(), args...);
+    }
+
+    template <class E, class... S>
+    template <class... Args>
+    inline auto xview<E, S...>::operator()(Args... args) const -> const_reference
+    {
+        return access_impl(std::make_index_sequence<sizeof...(Args) + squeeze_count<S...>()>(), args...);
+    }
+
+    template <class E, class... S>
+    template <typename E::size_type... I, class... Args>
+    inline auto xview<E, S...>::access_impl(std::index_sequence<I...>, Args... args) -> reference
+    {
+        return m_e(index<I>(args...)...);
+    }
+
+    template <class E, class... S>
+    template <typename E::size_type... I, class... Args>
+    inline auto xview<E, S...>::access_impl(std::index_sequence<I...>, Args... args) const -> const_reference
+    {
+        return m_e(index<I>(args...)...);
+    }
+
+    template <class E, class... S>
+    template <typename E::size_type I, class... Args>
+    inline auto xview<E, S...>::index(Args... args) const -> std::enable_if_t<(I<sizeof...(S)), size_type>
+    {
+        return sliced_access<I - squeeze_count_before<S...>(I)>(std::get<I>(m_slices), args...);
+    }
+
+    template <class E, class... S>
+    template <typename E::size_type I, class... Args>
+    inline auto xview<E, S...>::index(Args... args) const -> std::enable_if_t<(I>=sizeof...(S)), size_type>
+    {
+        return argument<I - squeeze_count_before<S...>(I)>(args...);
+    }
+
+    template <class E, class... S>
+    template<typename E::size_type I, class T, class... Args>
+    inline auto xview<E, S...>::sliced_access(const xslice<T>& slice, Args... args) const -> size_type
+    {
+        return slice.derived_cast()(argument<I>(args...));
+    }
+
+    template <class E, class... S>
+    template<typename E::size_type I, class T, class... Args>
+    inline auto xview<E, S...>::sliced_access(const T& squeeze, Args...) const -> disable_xslice<T, size_type>
+    {
+        return squeeze;
+    }
+
+    template <class E, class... S>
+    inline xview<E, S...> make_xview(E& e, S&&... slices)
+    {
+        return xview<E, S...>(std::forward<E>(e), std::forward<S>(slices)...);
+    }
+
+    /*********************************
+     * number of integral types
      *********************************/
 
     namespace detail
@@ -41,20 +219,21 @@ namespace qs
     }
 
     template <class... S>
-    constexpr size_t squeeze_count()
+    inline constexpr size_t squeeze_count()
     {
         return detail::squeeze_count_before_impl<S..., void>::count(sizeof...(S));
     }
 
     template <class... S>
-    constexpr size_t squeeze_count_before(size_t i)
+    inline constexpr size_t squeeze_count_before(size_t i)
     {
         return detail::squeeze_count_before_impl<S..., void>::count(i);
     }
 
     /**************************************
-     * index of n-th non-squeeze
+     * index of ith non-integral type
      **************************************/
+
     namespace detail
     {
 
@@ -78,133 +257,9 @@ namespace qs
     }
 
     template <class... S>
-    constexpr size_t non_squeeze(size_t i)
+    inline constexpr size_t non_squeeze(size_t i)
     {
         return detail::non_squeeze_impl<S..., void>::count(i);
-    }
-
-    /******************************************
-     * Views on xexpressions
-     ******************************************/
-
-    // Initialize an xview with an xexpression and a list of slices
-
-    // The xview is valid as long as the underlying expression has not
-    // been reshaped.
-
-    // If more slices are provided than the dimension of the underlying
-    // expression, the behavior is undefined.
-
-    template <class E, class... S>
-    class xview : public xexpression<xview<E, S...>>
-    {
-
-    public:
-
-        using self_type = xview<E, S...>;
-
-        using value_type = typename E::value_type;
-        using reference = typename E::reference;
-        using const_reference = typename E::const_reference;
-        using pointer = typename E::pointer;
-        using const_pointer = typename E::const_pointer;
-        using size_type = typename E::size_type;
-        using difference_type = typename E::difference_type;
-
-        using shape_type = xshape<size_type>;
-        using closure_type = const self_type&;
-
-        xview(E&& e, S&&... slices) noexcept
-            : m_e(e), m_slices(slices...)
-        {
-            constexpr size_t last_slice_index = sizeof...(S) - squeeze_count<S...>();
-            auto func = [](auto s) { return get_size(s); };
-            m_shape.resize(dimension());
-            for (size_type i = 0; i != last_slice_index; ++i)
-            {
-                m_shape[i] = apply<size_t>(non_squeeze<S...>(i), func, std::forward<S>(slices)...);
-            }
-            for (size_type i = last_slice_index; i != dimension(); ++i)
-            {
-                m_shape[i] = m_e.shape()[non_squeeze<S...>(i)];
-            }
-        }
-
-        inline size_type dimension() const noexcept
-        {
-            return m_e.dimension() - squeeze_count<S...>();
-        }
-        
-        auto shape() const noexcept
-        {
-            return m_shape;
-        }
-
-        inline bool broadcast_shape(shape_type& shape) const
-        {
-            return false;
-        }
-
-        template <class... Args>
-        reference operator()(Args... args)
-        {
-            return access_impl(std::make_index_sequence<sizeof...(Args) + squeeze_count<S...>()>(), args...);
-        }
-
-        template <class... Args>
-        const_reference operator()(Args... args) const
-        {
-            return access_impl(std::make_index_sequence<sizeof...(Args) + squeeze_count<S...>()>(), args...);
-        }
-
-    private:
-
-        E& m_e;
-        std::tuple<S...> m_slices;
-        shape_type m_shape;
-
-        template <size_type... I, class... Args>
-        reference access_impl(std::index_sequence<I...>, Args... args)
-        {
-            return m_e(index<I>(args...)...);
-        }
-
-        template <size_type... I, class... Args>
-        const_reference access_impl(std::index_sequence<I...>, Args... args) const
-        {
-            return m_e(index<I>(args...)...);
-        }
-
-        template <size_type I, class... Args>
-        std::enable_if_t<(I<sizeof...(S)), size_type> index(Args... args) const
-        {
-            return sliced_access<I - squeeze_count_before<S...>(I)>(std::get<I>(m_slices), args...);
-        }
-
-        template <size_type I, class... Args>
-        std::enable_if_t<(I>=sizeof...(S)), size_type> index(Args... args) const
-        {
-            return argument<I - squeeze_count_before<S...>(I)>(args...);
-        }
-
-        template<size_type I, class T, class... Args>
-        size_type sliced_access(const xslice<T>& slice, Args... args) const
-        {
-            return slice.derived_cast()(argument<I>(args...));
-        }
-
-        template<size_type I, class T, class... Args>
-        disable_xslice<T, size_type> sliced_access(const T& squeeze, Args...) const
-        {
-            return squeeze;
-        }
-
-    };
-
-    template <class E, class... S>
-    xview<E, S...> make_xview(E& e, S&&... slices)
-    {
-        return xview<E, S...>(std::forward<E>(e), std::forward<S>(slices)...);
     }
 
 }
