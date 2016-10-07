7134aa04f55cb25ff9d2135d2cee4634103e30b6
Sylvain Corlay <sylvain.corlay@gmail.com>
2016-10-07 19:27:02 +0200

    xslice cleanup

--- include/xarray/xslice.hpp
+++ include/xarray/xslice.hpp
@@ -8,6 +8,10 @@
 namespace qs
 {
 
+    /****************************
+     * xslice declaration
+     ****************************/
+
     template <class D>
     class xslice
     {
@@ -31,6 +35,82 @@ namespace qs
         xslice& operator=(xslice&&) = default;
     };
 
+    template <class S>
+    using is_xslice = std::is_base_of<xslice<S>, S>;
+
+    template <class E, class R>
+    using disable_xslice = typename std::enable_if<!is_xslice<E>::value, R>::type;
+
+    template <class... E>
+    using has_xslice = or_<is_xslice<E>...>;
+
+    /****************************
+     * xrange declaration
+     ****************************/
+
+    template <class size_type>
+    class xrange : public xslice<xrange<size_type>>
+    {
+
+    public:
+
+        xrange() = default;
+        explicit xrange(size_type min, size_type max) noexcept;
+
+        size_type operator()(size_type i) const noexcept;
+
+        size_type size() const noexcept;
+
+    private:
+
+        size_type m_min;
+        size_type m_size;
+    };
+
+    template <class size_type>
+    auto range(size_type min, size_type max)
+    {
+        return xrange<size_type>(min, max);
+    }
+
+    /********************************
+     * xall declaration
+     ********************************/
+
+    template <class size_type>
+    class xall : public xslice<xall<size_type>>
+    {
+
+    public:
+
+        xall() = default;
+        explicit xall(size_type size) noexcept;
+
+        size_type operator()(size_type i) const noexcept;
+
+        size_type size() const noexcept;
+
+    private:
+
+        size_type m_size;
+    };
+
+    /******************************************************
+     * homogeneous get_size for integral types and slices
+     ******************************************************/
+
+    template <class S>
+    inline disable_xslice<S, size_t> get_size(const S&)
+    {
+        return 0;
+    };
+
+    template <class S>
+    inline size_t get_size(const xslice<S>& slice)
+    {
+        return slice.derived_cast().size();
+    };
+
     /*********************************
      * xslice implementation
      *********************************/
@@ -47,103 +127,47 @@ namespace qs
         return *static_cast<const derived_type*>(this);
     }
 
-    template <class S>
-    using is_xslice = std::is_base_of<xslice<S>, S>;
-
-    template <class E, class R>
-    using disable_xslice = typename std::enable_if<!is_xslice<E>::value, R>::type;
-
-    template <class... E>
-    using has_xslice = or_<is_xslice<E>...>;
-
     /****************************
-     * xrange slice
+     * xrange implementation
      ****************************/
 
     template <class size_type>
-    class xrange : public xslice<xrange<size_type>>
+    inline xrange<size_type>::xrange(size_type min, size_type max) noexcept : m_min(min), m_size(max - min)
     {
-
-    public:
-
-        xrange() = default;
-        ~xrange() = default;
-        xrange(const xrange&) = default;
-        xrange& operator=(const xrange&) = default;
-        xrange(xrange&&) = default;
-        xrange& operator=(xrange&&) = default;
-
-        explicit xrange(size_type min, size_type max) noexcept : m_min(min), m_size(max - min) {}
-
-        inline size_type operator()(size_type i) const noexcept
-        {
-            return m_min + i;
-        }
-
-        inline size_type size() const noexcept
-        {
-            return m_size;
-        }
-
-    private:
-        size_type m_min;  
-        size_type m_size;  
-    };
+    }
 
     template <class size_type>
-    auto range(size_type min, size_type max)
+    inline size_type xrange<size_type>::operator()(size_type i) const noexcept
     {
-        return xrange<size_type>(min, max);
+        return m_min + i;
+    }
+
+    template <class size_type>
+    inline size_type xrange<size_type>::size() const noexcept
+    {
+        return m_size;
     }
 
     /********************************
-     * xall
+     * xall implementation
      ********************************/
 
     template <class size_type>
-    class xall : public xslice<xall<size_type>>
+    inline xall<size_type>::xall(size_type size) noexcept : m_size(size)
     {
+    }
 
-    public:
-
-        xall() = default;
-        ~xall() = default;
-        xall(const xall&) = default;
-        xall& operator=(const xall&) = default;
-        xall(xall&&) = default;
-        xall& operator=(xall&&) = default;
-
-        explicit xall(size_type size) noexcept : m_size(size) {}
-
-        inline size_type operator()(size_type i) const noexcept
-        {
-            return i;
-        }
-
-        inline size_type size() const noexcept
-        {
-            return m_size;
-        }
-
-    private:
-        size_type m_size;
-    };
-
-    /**********************************************
-     * Homogeneous get_size for slices and squeeze
-     **********************************************/
-
-    template <class S>
-    disable_xslice<S, size_t> get_size(const S&)
+    template <class size_type>
+    inline size_type xall<size_type>::operator()(size_type i) const noexcept
     {
-        return 0;
-    };
+        return i;
+    }
 
-    template <class S>
-    size_t get_size(const xslice<S>& slice)
+    template <class size_type>
+    inline size_type xall<size_type>::size() const noexcept
     {
-        return slice.derived_cast().size();
-    };
+        return m_size;
+    }
 
 }
 
