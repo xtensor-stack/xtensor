c94c2f0ee5dadf7d9b4234173234998e1357b457
Johan Mabille <johan.mabille@gmail.com>
2016-11-09 01:38:34 +0100

    static tensor and tests

---
+++ test/test_xtensor.cpp
@@ -0,0 +1,153 @@
+/***************************************************************************
+* Copyright (c) 2016, Johan Mabille and Sylvain Corlay                     *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#include "gtest/gtest.h"
+#include "xtensor/xtensor.hpp"
+#include "test_common.hpp"
+
+namespace xt
+{
+    using container_type = std::array<std::size_t, 3>;
+
+    TEST(xtensor, shaped_constructor)
+    {
+        {
+            SCOPED_TRACE("row_major constructor");
+            row_major_result<container_type> rm;
+            xtensor<int, 3> ra(rm.m_shape);
+            compare_shape(ra, rm);
+        }
+
+        {
+            SCOPED_TRACE("column_major constructor");
+            column_major_result<container_type> cm;
+            xtensor<int, 3> ca(cm.m_shape, layout::column_major);
+            compare_shape(ca, cm);
+        }
+    }
+
+    TEST(xtensor, strided_constructor)
+    {
+        central_major_result<container_type> cmr;
+        xtensor<int, 3> cma(cmr.m_shape, cmr.m_strides);
+        compare_shape(cma, cmr);
+    }
+
+    TEST(xtensor, valued_constructor)
+    {
+        {
+            SCOPED_TRACE("row_major valued constructor");
+            row_major_result<container_type> rm;
+            int value = 2;
+            xtensor<int, 3> ra(rm.m_shape, value);
+            compare_shape(ra, rm);
+            xtensor<int, 3>::container_type vec(ra.size(), value);
+            EXPECT_EQ(ra.data(), vec);
+        }
+
+        {
+            SCOPED_TRACE("column_major valued constructor");
+            column_major_result<container_type> cm;
+            int value = 2;
+            xtensor<int, 3> ca(cm.m_shape, value, layout::column_major);
+            compare_shape(ca, cm);
+            xtensor<int, 3>::container_type vec(ca.size(), value);
+            EXPECT_EQ(ca.data(), vec);
+        }
+    }
+
+    TEST(xtensor, strided_valued_constructor)
+    {
+        central_major_result<container_type> cmr;
+        int value = 2;
+        xtensor<int, 3> cma(cmr.m_shape, cmr.m_strides, value);
+        compare_shape(cma, cmr);
+        xtensor<int, 3>::container_type vec(cma.size(), value);
+        EXPECT_EQ(cma.data(), vec);
+    }
+
+    TEST(xtensor, copy_semantic)
+    {
+        central_major_result<container_type> res;
+        int value = 2;
+        xtensor<int, 3> a(res.m_shape, res.m_strides, value);
+
+        {
+            SCOPED_TRACE("copy constructor");
+            xtensor<int, 3> b(a);
+            compare_shape(a, b);
+            EXPECT_EQ(a.data(), b.data());
+        }
+
+        {
+            SCOPED_TRACE("assignment operator");
+            row_major_result<container_type> r;
+            xtensor<int, 3> c(r.m_shape, 0);
+            EXPECT_NE(a.data(), c.data());
+            c = a;
+            compare_shape(a, c);
+            EXPECT_EQ(a.data(), c.data());
+        }
+    }
+
+    TEST(xtensor, move_semantic)
+    {
+        central_major_result<container_type> res;
+        int value = 2;
+        xtensor<int, 3> a(res.m_shape, res.m_strides, value);
+
+        {
+            SCOPED_TRACE("move constructor");
+            xtensor<int, 3> tmp(a);
+            xtensor<int, 3> b(std::move(tmp));
+            compare_shape(a, b);
+            EXPECT_EQ(a.data(), b.data());
+        }
+
+        {
+            SCOPED_TRACE("move assignment");
+            row_major_result<container_type> r;
+            xtensor<int, 3> c(r.m_shape, 0);
+            EXPECT_NE(a.data(), c.data());
+            xtensor<int, 3> tmp(a);
+            c = std::move(tmp);
+            compare_shape(a, c);
+            EXPECT_EQ(a.data(), c.data());
+        }
+    }
+
+    TEST(xtensor, reshape)
+    {
+        xtensor<int, 3> a;
+        test_reshape<xtensor<int, 3>, container_type>(a);
+    }
+
+    TEST(xtensor, access)
+    {
+        xtensor<int, 3> a;
+        test_access<xtensor<int, 3>, container_type>(a);
+    }
+
+    TEST(xtensor, broadcast_shape)
+    {
+        xtensor<int, 4> a;
+        test_broadcast(a);
+    }
+
+    TEST(xtensor, storage_iterator)
+    {
+        xtensor<int, 3> a;
+        test_storage_iterator<xtensor<int, 3>, container_type>(a);
+    }
+
+    TEST(xtensor, zerod)
+    {
+        xtensor<int, 3> a;
+        EXPECT_EQ(0, a());
+    }
+}
---
+++ test/test_xtensor_adaptor.cpp
@@ -0,0 +1,127 @@
+/***************************************************************************
+* Copyright (c) 2016, Johan Mabille and Sylvain Corlay                     *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#include "gtest/gtest.h"
+#include "xtensor/xtensor.hpp"
+#include "test_common.hpp"
+
+namespace xt
+{
+    using vec_type = std::vector<int>;
+    using adaptor_type = xtensor_adaptor<vec_type, 3>;
+    using container_type = std::array<std::size_t, 3>;
+
+    TEST(xtensor_adaptor, shaped_constructor)
+    {
+        {
+            SCOPED_TRACE("row_major constructor");
+            row_major_result<container_type> rm;
+            vec_type v;
+            adaptor_type a(v, rm.shape());
+            compare_shape(a, rm);
+        }
+
+        {
+            SCOPED_TRACE("column_major constructor");
+            column_major_result<container_type> cm;
+            vec_type v;
+            adaptor_type a(v, cm.shape(), layout::column_major);
+            compare_shape(a, cm);
+        }
+    }
+
+    TEST(xtensor_adaptor, strided_constructor)
+    {
+        central_major_result<container_type> cmr;
+        vec_type v;
+        adaptor_type a(v, cmr.shape(), cmr.strides());
+        compare_shape(a, cmr);
+    }
+
+    TEST(xtensor_adaptor, copy_semantic)
+    {
+        central_major_result<container_type> res;
+        int value = 2;
+        vec_type v(res.size(), value);
+        adaptor_type a(v, res.shape(), res.strides());
+
+        {
+            SCOPED_TRACE("copy constructor");
+            adaptor_type b(a);
+            compare_shape(a, b);
+            EXPECT_EQ(a.data(), b.data());
+        }
+
+        {
+            SCOPED_TRACE("assignment operator");
+            row_major_result<container_type> r;
+            vec_type v(r.size(), 0);
+            adaptor_type c(v, r.shape());
+            EXPECT_NE(a.data(), c.data());
+            c = a;
+            compare_shape(a, c);
+            EXPECT_EQ(a.data(), c.data());
+        }
+    }
+
+    TEST(xtensor_adaptor, move_semantic)
+    {
+        central_major_result<container_type> res;
+        int value = 2;
+        vec_type v(res.size(), value);
+        adaptor_type a(v, res.shape(), res.strides());
+
+        {
+            SCOPED_TRACE("move constructor");
+            adaptor_type tmp(a);
+            adaptor_type b(std::move(tmp));
+            compare_shape(a, b);
+            EXPECT_EQ(a.data(), b.data());
+        }
+
+        {
+            SCOPED_TRACE("move assignment");
+            row_major_result<container_type> r;
+            vec_type v(r.size(), 0);
+            adaptor_type c(v, r.shape());
+            EXPECT_NE(a.data(), c.data());
+            adaptor_type tmp(a);
+            c = std::move(tmp);
+            compare_shape(a, c);
+            EXPECT_EQ(a.data(), c.data());
+        }
+    }
+
+    TEST(xtensor_adaptor, reshape)
+    {
+        vec_type v;
+        adaptor_type a(v);
+        test_reshape<adaptor_type, container_type>(a);
+    }
+
+    TEST(xtensor_adaptor, access)
+    {
+        vec_type v;
+        adaptor_type a(v);
+        test_access<adaptor_type, container_type>(a);
+    }
+
+    TEST(xtensor_adaptor, broadcast_shape)
+    {
+        vec_type v;
+        xtensor_adaptor<vec_type, 4> a(v);
+        test_broadcast(a);
+    }
+
+    TEST(xtensor_adaptor, storage_iterator)
+    {
+        vec_type v;
+        adaptor_type a(v);
+        test_storage_iterator<adaptor_type, container_type>(a);
+    }
+}
