9694281f46586ce7b3db22f1836a5b1221c4253f
Wolf Vollprecht <w.vollprecht@gmail.com>
2016-12-17 19:51:32 +0100

    change tabs to spaces

--- include/xtensor/xblas.hpp
+++ include/xtensor/xblas.hpp
@@ -23,68 +23,69 @@ namespace blas
      * @param b vector of n elements
      * @returns scalar result
      */
-	template <class T>
-	xscalar<T> dot(const xexpression<T>& a, const xexpression<T>& b)
-	{
-		T res;
-		cxxblas::dot(a.data().size(), 
-					 a.data().data() + a.offset(), a.strides()[0], 
-					 b.data().data() + b.offset(), b.strides()[0], 
-					 res);
-		return res;
-	}
+    template <class T>
+    xscalar<T> dot(const xexpression<T>& a, const xexpression<T>& b)
+    {
+        T res;
+        cxxblas::dot(a.data().size(), 
+                     a.data().data() + a.offset(), a.strides()[0], 
+                     b.data().data() + b.offset(), b.strides()[0], 
+                     res);
+        return res;
+    }
 
-	/**
-	 * Calculate the matrix-vector product of matrix @a and vector @b
-	 * @param a matrix of m-by-n elements
-	 * @param b vector of n elements
-	 * @returns vector of n elements
-	 */
-	template <class E1, class E2>
-	E1 gemv(const xexpression<E1>& A, const xexpression<E2>& y)
-	{
-		const E1& dA = A.derived_cast();
-		const E2& dy = y.derived_cast();
-		xt::xarray<typename E1::value_type> res(dy.shape());
+    /**
+     * Calculate the matrix-vector product of matrix @a and vector @b
+     * @param a matrix of m-by-n elements
+     * @param b vector of n elements
+     * @returns vector of n elements
+     */
+    template <class E1, class E2>
+    E1 gemv(const xexpression<E1>& A, const xexpression<E2>& y)
+    {
+        const E1& dA = A.derived_cast();
+        const E2& dy = y.derived_cast();
+        xt::xarray<typename E1::value_type> res(dy.shape());
 
-		cxxblas::gemv(
-			cxxblas::StorageOrder::RowMajor, 
-			cxxblas::Transpose::NoTrans, 
-			dA.shape()[0], dA.shape()[1],
-			1.f, // alpha
-			dA.data().data(), dA.strides()[0],
-			dy.data().data(), dy.strides()[0], 
-			0.f, // beta 
-			res.data().data(), 1ul);
+        cxxblas::gemv(
+            cxxblas::StorageOrder::RowMajor, 
+            cxxblas::Transpose::NoTrans, 
+            dA.shape()[0], dA.shape()[1],
+            1.f, // alpha
+            dA.data().data(), dA.strides()[0],
+            dy.data().data(), dy.strides()[0], 
+            0.f, // beta 
+            res.data().data(), 1ul);
 
-		return res;
-	}
+        return res;
+    }
 
-	/**
-	 * Calculate the matrix-matrix product of matrix @A and matrix @B
-	 * @param A matrix of m-by-n elements
-	 * @param B matrix of n-by-k elements
-	 * @returns matrix of m-by-k elements
-	 */
-	template <class E1, class E2>
-	E1 gemm(const xexpression<E1>& A, const xexpression<E2>& B)
-	{
-		const E1& da = A.derived_cast();
-		const E2& db = B.derived_cast();
-		
-		E2 res(db.shape());
-		cxxblas::gemm(
-			cxxblas::StorageOrder::RowMajor, 
-			cxxblas::Transpose::NoTrans, cxxblas::Transpose::NoTrans, 
-			da.shape()[0], da.shape()[1], db.shape()[0],
-			1.f, // alpha
-			da.data().data(), da.strides()[0],
-			db.data().data(), db.strides()[0], 
-			0.f, // beta
-			res.data().data(), res.strides()[0]);
-		return res;
-	}
+    /**
+     * Calculate the matrix-matrix product of matrix @A and matrix @B
+     * @param A matrix of m-by-n elements
+     * @param B matrix of n-by-k elements
+     * @returns matrix of m-by-k elements
+     */
+    template <class E1, class E2>
+    E1 gemm(const xexpression<E1>& A, const xexpression<E2>& B)
+    {
+        const E1& da = A.derived_cast();
+        const E2& db = B.derived_cast();
+        
+        E2 res(db.shape());
+        cxxblas::gemm(
+            cxxblas::StorageOrder::RowMajor, 
+            cxxblas::Transpose::NoTrans, cxxblas::Transpose::NoTrans, 
+            da.shape()[0], da.shape()[1], db.shape()[0],
+            1.f, // alpha
+            da.data().data(), da.strides()[0],
+            db.data().data(), db.strides()[0], 
+            0.f, // beta
+            res.data().data(), res.strides()[0]);
+        return res;
+    }
 }
 
 }
+
 #endif
