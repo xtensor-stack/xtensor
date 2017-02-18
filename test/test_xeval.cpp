38543e7ad717b5eec3192eca3d004a212f14eef8
Wolf Vollprecht <w.vollprecht@gmail.com>
2017-02-18 11:25:32 +0100

    fix test on clang

--- test/test_xeval.cpp
+++ test/test_xeval.cpp
@@ -8,6 +8,7 @@
 
 #include "gtest/gtest.h"
 
+#include "xtensor/xtensor_config.hpp"
 #include "xtensor/xeval.hpp"
 #include "xtensor/xbuilder.hpp"
 
@@ -44,9 +45,17 @@ namespace xt
         bool type_eq = std::is_same<decltype(b), xarray<double>&&>::value;
         EXPECT_TRUE(type_eq);
 
+        xtensor<int, 2> k({3, 3});
+        auto m = k * k - 4;
+        auto&& n = eval(m);
+        bool type_eq_3 = std::is_same<decltype(n), xtensor<int, 2>&&>::value;
+        EXPECT_TRUE(type_eq_3);
+
+#ifndef X_OLD_CLANG
         auto&& i = eval(linspace(0, 100));
         bool type_eq_2 = std::is_same<decltype(i), xtensor<int, 1>&&>::value;
         EXPECT_TRUE(type_eq_2);
+#endif
     }
 }
 
