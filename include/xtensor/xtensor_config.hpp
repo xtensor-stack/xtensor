0bee2b31cd5507b5295b29c13fff72797fbf1815
Sylvain Corlay <sylvain.corlay@gmail.com>
2017-07-27 10:54:53 +0200

    Release 0.10.9

--- include/xtensor/xtensor_config.hpp
+++ include/xtensor/xtensor_config.hpp
@@ -11,7 +11,7 @@
 
 #define XTENSOR_VERSION_MAJOR 0
 #define XTENSOR_VERSION_MINOR 10
-#define XTENSOR_VERSION_PATCH 8
+#define XTENSOR_VERSION_PATCH 9
 
 // DETECT 3.6 <= clang < 3.8 for compiler bug workaround.
 #ifdef __clang__
