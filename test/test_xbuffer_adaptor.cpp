0ae8f341b6b972713cf2d93f3cd98b2130f20300
Sylvain Corlay <sylvain.corlay@gmail.com>
2017-10-26 07:45:20 +0200

    Buffer adaptor acquires closure on passed pointer

--- test/test_xbuffer_adaptor.cpp
+++ test/test_xbuffer_adaptor.cpp
@@ -11,8 +11,8 @@
 
 namespace xt
 {
-    using buffer_adaptor = xbuffer_adaptor<double>;
-    using owner_adaptor = xbuffer_adaptor<double, acquire_ownership>;
+    using buffer_adaptor = xbuffer_adaptor<double*>;
+    using owner_adaptor = xbuffer_adaptor<double*&, acquire_ownership>;
 
     TEST(xbuffer_adaptor, owner_destructor)
     {
