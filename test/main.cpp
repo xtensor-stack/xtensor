c21dfcb2fde0ea636d59385047fdda8405943454
Sylvain Corlay <sylvain.corlay@gmail.com>
2017-11-05 15:40:31 +0100

    ..

--- test/main.cpp
+++ test/main.cpp
@@ -6,7 +6,7 @@
 * The full license is in the file LICENSE, distributed with this software. *
 ****************************************************************************/
 
-#include "xtensor/xtensor.hpp"
+#include "xtensor/xcontainer.hpp"
 
 int main(int argc, char* argv[])
 {
