b688827fe4cc621204efa7f328e30f708cf8e2af
Johan Mabille <johan.mabille@gmail.com>
2018-03-22 05:23:31 +0100

    testing xtensor with SIMD enabled

--- test/test_xfixed.cpp
+++ test/test_xfixed.cpp
@@ -12,6 +12,16 @@
 #include "xtensor/xarray.hpp"
 #include "xtensor/xtensor.hpp"
 
+// On VS2015, when compiling in x86 mode, alignas(T) leads to C2718
+// when used for a function parameter, even indirectly. This means that
+// we cannot pass parameters whose class is declared with alignas specifier
+// or any type wrapping or inheriting from such a type.
+// The xtensorf class internally uses aligned_array which is declared as
+// alignas(something_different_from_0), hence the workaround.
+#if _MSC_VER < 1910 && !_WIN64
+#define VS_X86_WORKAROUND 1
+#endif
+
 #if _MSC_VER < 1910 || (_MSC_VER >= 1910 && !defined(DISABLE_VS2017))
 
 namespace xt
@@ -27,7 +37,12 @@ namespace xt
         xtensorf3x3 res1 = a + b;
         xtensorf3x3 res2 = 2.0 * a;
 
+#ifndef VS_X86_WORKAROUND
         EXPECT_EQ(res1, res2);
+#else
+        bool res = std::equal(res1.cbegin(), res1.cend(), res2.cbegin());
+        EXPECT_TRUE(res);
+#endif
     }
 
     TEST(xtensorf, broadcast)
@@ -43,8 +58,15 @@ namespace xt
         xarray<double> arx = a * b;
         xarray<double> brx = b * a;
 
+#ifndef VS_X86_WORKAROUND
         EXPECT_EQ(res, arx);
         EXPECT_EQ(resb, brx);
+#else
+        bool bresa = std::equal(res.cbegin(), res.cend(), arx.cbegin());
+        EXPECT_TRUE(bresa);
+        bool bresb = std::equal(resb.cbegin(), resb.cend(), brx.cbegin());
+        EXPECT_TRUE(bresb);
+#endif
 
 #ifdef XTENSOR_ENABLE_ASSERT
         EXPECT_THROW(a.resize({2, 2}), std::runtime_error);
