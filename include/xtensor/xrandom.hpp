92bf5be3da03c2ab25bdedf03ec69541f1f70690
Kotone Itaya <kotone@sfc.keio.ac.jp>
2018-09-06 17:22:30 +0900

    Modify uniform_int_distribution template type to be size_type

--- include/xtensor/xrandom.hpp
+++ include/xtensor/xrandom.hpp
@@ -261,12 +261,13 @@ namespace xt
 
             if (de.dimension() == 1)
             {
+                using size_type = typename T::size_type;
                 auto first = de.storage().begin();
                 auto last = de.storage().end();
 
-                for (auto i = (last - first) - 1; i > 0; --i)
+                for (size_type i = (last - first) - 1; i > 0; --i)
                 {
-                    std::uniform_int_distribution<decltype(i)> dist(0, i);
+                    std::uniform_int_distribution<size_type> dist(0, i);
                     auto j = dist(engine);
                     using std::swap;
                     swap(first[i], first[j]);
