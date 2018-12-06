e6354ad21755b6a5a28e924de0389947ce29155a
marty1885 <marty188586@gmail.com>
2018-12-06 13:00:55 +0800

    fix style

--- include/xtensor/xcsv.hpp
+++ include/xtensor/xcsv.hpp
@@ -53,7 +53,7 @@ namespace xt
         inline std::string lexical_cast(const std::string& cell)
         {
             size_t first = cell.find_first_not_of(' ');
-            if(first == std::string::npos)
+            if (first == std::string::npos)
                 return cell;
 
             size_t last = cell.find_last_not_of(' ');
