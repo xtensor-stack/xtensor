bc38e11d0e7512e36f809effd3f60f238081804b
martinRenou <martin.renou@gmail.com>
2019-01-10 11:04:39 +0100

    Fix xmasked_view assign

--- test/test_xmasked_view.cpp
+++ test/test_xmasked_view.cpp
@@ -219,4 +219,25 @@ namespace xt
                                     {6.65, 8.  , 6.65}};
         EXPECT_EQ(data, expected2);
     }
+
+    TEST(xmasked_view, assign)
+    {
+        xarray<double> data = {{ 1.,-2., 3.},
+                               { 4., 5.,-6.},
+                               { 7., 8.,-9.}};
+        xarray<double> data2 = {{ 0.1, 0.2, 0.3},
+                                { 0.4, 0.5, 0.6},
+                                { 0.7, 0.8, 0.9}};
+        xarray<bool> mask = {{ true,  true,  true},
+                             { true, false, false},
+                             { true, false,  true}};
+
+        auto masked_data = masked_view(data, mask);
+
+        masked_data = data2;
+        xarray<double> expected1 = {{ 0.1, 0.2, 0.3},
+                                    { 0.4, 5. ,-6. },
+                                    { 0.7, 8. , 0.9}};
+        EXPECT_EQ(data, expected1);
+    }
 }
