cc93f098d384940cd2b8b58fb927037ca7c53593
Johan Mabille <johan.mabille@gmail.com>
2019-08-27 09:20:09 +0200

    Reset stream's flags after output operation

--- include/xtensor/xio.hpp
+++ include/xtensor/xio.hpp
@@ -702,9 +702,35 @@ namespace xt
         return pretty_print(print_fun, out);
     }
 
+    namespace detail
+    {
+        template <class S>
+        class fmtflags_guard
+        {
+        public:
+
+            explicit fmtflags_guard(S& stream)
+                : m_stream(stream), m_flags(stream.flags())
+            {
+            }
+
+            ~fmtflags_guard()
+            {
+                m_stream.flags(m_flags);
+            }
+
+        private:
+
+            S& m_stream;
+            std::ios_base::fmtflags m_flags;
+        };
+    }
+
     template <class E>
     std::ostream& pretty_print(const xexpression<E>& e, std::ostream& out = std::cout)
     {
+        detail::fmtflags_guard<std::ostream> guard(out);
+
         const E& d = e.derived_cast();
 
         std::size_t lim = 0;
