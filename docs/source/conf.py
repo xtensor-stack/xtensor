3feff8380b849927dafb51cf5dcf2898f23a1e5c
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-02-12 18:12:14 +0100

    add conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ import subprocess
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if on_rtd:
-    subprocess.call('cd ..; doxygen', shell=True)
+# if on_rtd:
+#     subprocess.call('cd ..; doxygen', shell=True)
 
 import sphinx_rtd_theme
 
@@ -15,20 +15,34 @@ html_theme = "sphinx_rtd_theme"
 
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+html_theme_options = {
+	'logo_only': True,
+    # 'collapse_navigation': False
+}
+
 def setup(app):
     app.add_stylesheet("main_stylesheet.css")
 
 extensions = ['breathe']
 breathe_projects = { 'xtensor': '../xml' }
 templates_path = ['_templates']
+
 html_static_path = ['_static']
+html_css_files = [
+    'css/stylesheet.css',
+]
 source_suffix = '.rst'
-master_doc = 'index'
+master_doc = 'docindex'
+html_additional_pages = {'index': 'index.html'}
+
+
 project = 'xtensor'
-copyright = '2016, Johan Mabille, Sylvain Corlay and Wolf Vollprecht'
+copyright = '2020, Johan Mabille, Sylvain Corlay and Wolf Vollprecht'
 author = 'Johan Mabille, Sylvain Corlay and Wolf Vollprecht'
 
-html_logo = 'quantstack-white.svg'
+logo = True
+theme_logo_only = True
+html_logo = 'xtensor.svg'
 
 exclude_patterns = []
 highlight_language = 'c++'
