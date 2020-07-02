f735e6c3f6b065531a21480c03ee18721428c835
David Brochart <david.brochart@gmail.com>
2020-07-02 15:23:37 +0200

    Add xchunked_array (#2076)
    Add xchunked_array

---
+++ test/test_xchunked_array.cpp
@@ -0,0 +1,25 @@
+/***************************************************************************
+* Copyright (c) Johan Mabille, Sylvain Corlay and Wolf Vollprecht          *
+* Copyright (c) QuantStack                                                 *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#include "gtest/gtest.h"
+#include "xtensor/xchunked_array.hpp"
+
+namespace xt
+{
+    using chunked_array = xt::xchunked_array<xt::xarray<double>>;
+
+    TEST(xchunked_array, indexed_access)
+    {
+        chunked_array a(
+            {10, 10, 10},
+            {2, 3, 4}
+        );
+        a(3, 9, 8);
+    }
+}
