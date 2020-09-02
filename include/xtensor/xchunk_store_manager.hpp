617885e931a4f848d6d6f9522338060268644816
David Brochart <david.brochart@gmail.com>
2020-09-02 11:09:26 +0200

    Add xindex_path to transform indexes into path

--- include/xtensor/xchunk_store_manager.hpp
+++ include/xtensor/xchunk_store_manager.hpp
@@ -11,38 +11,53 @@
 namespace xt
 {
 
+    /***************************
+     * xindex_path declaration *
+     ***************************/
+
+    class xindex_path
+    {
+    public:
+        void set_directory(const char* directory);
+        template <class I>
+        void index_to_path(I, I, std::string&);
+
+    private:
+        std::string m_directory;
+    };
+
     /************************************
      * xchunk_store_manager declaration *
      ************************************/
 
-    template <class EC>
+    template <class EC, class IP>
     class xchunk_store_manager;
 
-    template <class EC>
-    struct xcontainer_inner_types<xchunk_store_manager<EC>>
+    template <class EC, class IP>
+    struct xcontainer_inner_types<xchunk_store_manager<EC, IP>>
     {
         using storage_type = EC;
         using reference = EC&;
         using const_reference = const EC&;
         using size_type = std::size_t;
-        using temporary_type = xchunk_store_manager<EC>;
+        using temporary_type = xchunk_store_manager<EC, IP>;
     };
 
-    template <class EC>
-    struct xiterable_inner_types<xchunk_store_manager<EC>>
+    template <class EC, class IP>
+    struct xiterable_inner_types<xchunk_store_manager<EC, IP>>
     {
         using inner_shape_type = std::vector<std::size_t>;
-        using stepper = xindexed_stepper<xchunk_store_manager<EC>, false>;
-        using const_stepper = xindexed_stepper<xchunk_store_manager<EC>, true>;
+        using stepper = xindexed_stepper<xchunk_store_manager<EC, IP>, false>;
+        using const_stepper = xindexed_stepper<xchunk_store_manager<EC, IP>, true>;
     };
 
-    template <class EC>
-    class xchunk_store_manager: public xaccessible<xchunk_store_manager<EC>>,
-                                public xiterable<xchunk_store_manager<EC>>
+    template <class EC, class IP = xindex_path>
+    class xchunk_store_manager: public xaccessible<xchunk_store_manager<EC, IP>>,
+                                public xiterable<xchunk_store_manager<EC, IP>>
     {
     public:
 
-        using self_type = xchunk_store_manager<EC>;
+        using self_type = xchunk_store_manager<EC, IP>;
         using inner_types = xcontainer_inner_types<self_type>;
         using storage_type = typename inner_types::storage_type;
         using value_type = storage_type;
@@ -95,6 +110,7 @@ namespace xt
 
         void set_pool_size(std::size_t n);
         void set_directory(const char* directory);
+        IP& get_index_path();
         void flush();
 
         template <class C>
@@ -115,15 +131,44 @@ namespace xt
         chunk_pool_type m_chunk_pool;
         index_pool_type m_index_pool;
         std::size_t m_unload_index;
-        std::string m_directory;
+        IP m_index_path;
     };
+
+    /******************************
+     * xindex_path implementation *
+     ******************************/
+
+    void xindex_path::set_directory(const char* directory)
+    {
+        m_directory = directory;
+        if (m_directory.back() != '/')
+        {
+            m_directory.push_back('/');
+        }
+    }
+
+    template <class I>
+    void xindex_path::index_to_path(I first, I last, std::string& path)
+    {
+        std::string fname;
+        for (auto it = first; it != last; ++it)
+        {
+            if (!fname.empty())
+            {
+                fname.push_back('.');
+            }
+            fname.append(std::to_string(*it));
+        }
+        path = m_directory + fname;
+        std::cout << path << std::endl;
+    }
 
     /***************************************
      * xchunk_store_manager implementation *
      ***************************************/
 
-    template <class EC>
-    inline xchunk_store_manager<EC>::xchunk_store_manager()
+    template <class EC, class IP>
+    inline xchunk_store_manager<EC, IP>::xchunk_store_manager()
         : m_shape()
         // default pool size is 1
         // so that first chunk is always resized to the chunk shape
@@ -134,84 +179,84 @@ namespace xt
         m_chunk_pool[0].ignore_empty_path(true);
     }
 
-    template <class EC>
-    inline auto xchunk_store_manager<EC>::shape() const noexcept -> const shape_type&
+    template <class EC, class IP>
+    inline auto xchunk_store_manager<EC, IP>::shape() const noexcept -> const shape_type&
     {
         return m_shape;
     }
 
-    template <class EC>
+    template <class EC, class IP>
     template <class... Idxs>
-    inline auto xchunk_store_manager<EC>::operator()(Idxs... idxs) -> reference
+    inline auto xchunk_store_manager<EC, IP>::operator()(Idxs... idxs) -> reference
     {
         auto index = get_indexes(idxs...);
         return map_file_array(index.cbegin(), index.cend());
     }
 
-    template <class EC>
+    template <class EC, class IP>
     template <class... Idxs>
-    inline auto xchunk_store_manager<EC>::operator()(Idxs... idxs) const -> const_reference
+    inline auto xchunk_store_manager<EC, IP>::operator()(Idxs... idxs) const -> const_reference
     {
         auto index = get_indexes(idxs...);
         return map_file_array(index.cbegin(), index.cend());
     }
 
-    template <class EC>
+    template <class EC, class IP>
     template <class It>
-    inline auto xchunk_store_manager<EC>::element(It first, It last) -> reference
+    inline auto xchunk_store_manager<EC, IP>::element(It first, It last) -> reference
     {
         return map_file_array(first, last);
     }
 
-    template <class EC>
+    template <class EC, class IP>
     template <class It>
-    inline auto xchunk_store_manager<EC>::element(It first, It last) const -> const_reference
+    inline auto xchunk_store_manager<EC, IP>::element(It first, It last) const -> const_reference
     {
         return map_file_array(first, last);
     }
 
-    template <class EC>
+    template <class EC, class IP>
     template <class O>
-    inline auto xchunk_store_manager<EC>::stepper_begin(const O& shape) noexcept -> stepper
+    inline auto xchunk_store_manager<EC, IP>::stepper_begin(const O& shape) noexcept -> stepper
     {
         size_type offset = shape.size() - this->dimension();
         return stepper(this, offset);
     }
 
-    template <class EC>
+    template <class EC, class IP>
     template <class O>
-    inline auto xchunk_store_manager<EC>::stepper_end(const O& shape, layout_type) noexcept -> stepper
+    inline auto xchunk_store_manager<EC, IP>::stepper_end(const O& shape, layout_type) noexcept -> stepper
     {
         size_type offset = shape.size() - this->dimension();
         return stepper(this, offset, true);
     }
 
-    template <class EC>
+    template <class EC, class IP>
     template <class O>
-    inline auto xchunk_store_manager<EC>::stepper_begin(const O& shape) const noexcept -> const_stepper
+    inline auto xchunk_store_manager<EC, IP>::stepper_begin(const O& shape) const noexcept -> const_stepper
     {
         size_type offset = shape.size() - this->dimension();
         return const_stepper(this, offset);
     }
 
-    template <class EC>
+    template <class EC, class IP>
     template <class O>
-    inline auto xchunk_store_manager<EC>::stepper_end(const O& shape, layout_type) const noexcept -> const_stepper
+    inline auto xchunk_store_manager<EC, IP>::stepper_end(const O& shape, layout_type) const noexcept -> const_stepper
     {
         size_type offset = shape.size() - this->dimension();
         return const_stepper(this, offset, true);
     }
 
-    template <class EC>
+    template <class EC, class IP>
     template <class S>
-    inline void xchunk_store_manager<EC>::resize(S&&)
+    inline void xchunk_store_manager<EC, IP>::resize(S&&)
     {
         // don't resize according to total number of chunks
         // instead the pool manages a number of in-memory chunks
     }
 
-    template <class EC>
-    inline void xchunk_store_manager<EC>::set_pool_size(std::size_t n)
+    template <class EC, class IP>
+    inline void xchunk_store_manager<EC, IP>::set_pool_size(std::size_t n)
     {
         // first chunk always has the correct shape
         // get the shape before resizing the pool
@@ -227,8 +272,8 @@ namespace xt
         }
     }
 
-    template <class EC>
-    inline void xchunk_store_manager<EC>::flush()
+    template <class EC, class IP>
+    inline void xchunk_store_manager<EC, IP>::flush()
     {
         for (auto& chunk: m_chunk_pool)
         {
@@ -236,9 +281,9 @@ namespace xt
         }
     }
 
-    template <class EC>
+    template <class EC, class IP>
     template <class C>
-    void xchunk_store_manager<EC>::configure_format(C& config)
+    void xchunk_store_manager<EC, IP>::configure_format(C& config)
     {
         for (auto& chunk: m_chunk_pool)
         {
@@ -246,41 +291,33 @@ namespace xt
         }
     }
 
-    template <class EC>
-    void xchunk_store_manager<EC>::set_directory(const char* directory)
+    template <class EC, class IP>
+    IP& xchunk_store_manager<EC, IP>::get_index_path()
     {
-        m_directory = directory;
-        if (m_directory.back() != '/')
-        {
-            m_directory.append("/");
-        }
+        return m_index_path;
     }
 
-    template <class EC>
+    template <class EC, class IP>
+    void xchunk_store_manager<EC, IP>::set_directory(const char* directory)
+    {
+        m_index_path.set_directory(directory);
+    }
+
+    template <class EC, class IP>
     template <class I>
-    inline auto xchunk_store_manager<EC>::map_file_array(I first, I last) -> reference
+    inline auto xchunk_store_manager<EC, IP>::map_file_array(I first, I last) -> reference
     {
         std::string path;
-        std::string fname;
-        std::vector<std::size_t> index;
-        for (auto it = first; it != last; ++it)
-        {
-            if (!fname.empty())
-            {
-                fname.append(".");
-            }
-            fname.append(std::to_string(*it));
-            index.push_back(*it);
-        }
-        path = m_directory + fname;
-        if (index.empty())
+        m_index_path.index_to_path(first, last, path);
+        if (first == last)
         {
             return m_chunk_pool[0];
         }
         else
         {
             // check if the chunk is already loaded in memory
-            const auto it1 = std::find(m_index_pool.cbegin(), m_index_pool.cend(), index);
+            const auto it1 = std::find_if(m_index_pool.cbegin(), m_index_pool.cend(), [first, last](const auto& v)
+                { return std::equal(v.cbegin(), v.cend(), first, last); });
             std::size_t i;
             if (it1 != m_index_pool.cend())
             {
@@ -294,23 +331,25 @@ namespace xt
             {
                 i = std::distance(m_index_pool.cbegin(), it2);
                 m_chunk_pool[i].set_path(path);
-                m_index_pool[i] = index;
+                m_index_pool[i].resize(static_cast<size_t>(std::distance(first, last)));
+                std::copy(first, last, m_index_pool[i].begin());
                 return m_chunk_pool[i];
             }
             // no free chunk, take one (which will thus be unloaded)
             // fairness is guaranteed through the use of a walking index
             m_chunk_pool[m_unload_index].set_path(path);
-            m_index_pool[m_unload_index] = index;
+            m_index_pool[m_unload_index].resize(static_cast<size_t>(std::distance(first, last)));
+            std::copy(first, last, m_index_pool[m_unload_index].begin());
             auto& chunk = m_chunk_pool[m_unload_index];
             m_unload_index = (m_unload_index + 1) % m_index_pool.size();
             return chunk;
         }
     }
 
-    template <class EC>
+    template <class EC, class IP>
     template <class... Idxs>
     inline std::array<std::size_t, sizeof...(Idxs)>
-    xchunk_store_manager<EC>::get_indexes(Idxs... idxs) const
+    xchunk_store_manager<EC, IP>::get_indexes(Idxs... idxs) const
     {
         std::array<std::size_t, sizeof...(Idxs)> indexes = {{idxs...}};
         return indexes;
