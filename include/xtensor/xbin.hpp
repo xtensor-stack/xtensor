a5fe06315c5c762f507b5c5018356e77300e0cb6
David Brochart <david.brochart@gmail.com>
2020-09-08 10:13:45 +0200

    Add xbin for plain binary storage

---
+++ include/xtensor/xbin.hpp
@@ -0,0 +1,143 @@
+/***************************************************************************
+* Copyright (c) Wolf Vollprecht, Sylvain Corlay and Johan Mabille          *
+* Copyright (c) QuantStack                                                 *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#ifndef XTENSOR_IO_BIN_HPP
+#define XTENSOR_IO_BIN_HPP
+
+#include <fstream>
+
+#include "xtensor/xadapt.hpp"
+
+namespace xt
+{
+    namespace detail
+    {
+        template <typename T>
+        inline std::vector<T> load_bin_file(std::istream& stream)
+        {
+            stream.seekg(0, stream.end);
+            auto uncompressed_size = static_cast<std::size_t>(stream.tellg());
+            stream.seekg(0, stream.beg);
+            std::vector<T> uncompressed_buffer(uncompressed_size / sizeof(T));
+            stream.read(reinterpret_cast<char*>(uncompressed_buffer.data()), (std::streamsize)uncompressed_size);
+            return uncompressed_buffer;
+        }
+
+        template <class O, class E>
+        inline void dump_bin_stream(O& stream, const xexpression<E>& e)
+        {
+            using value_type = typename E::value_type;
+            const E& ex = e.derived_cast();
+            auto&& eval_ex = eval(ex);
+            auto shape = eval_ex.shape();
+            std::size_t uncompressed_size = compute_size(shape) * sizeof(value_type);
+            const char* uncompressed_buffer = reinterpret_cast<const char*>(eval_ex.data());
+            stream.write(uncompressed_buffer, std::streamsize(uncompressed_size));
+            stream.flush();
+        }
+    }  // namespace detail
+
+    /**
+     * Save xexpression to BIN format
+     *
+     * @param stream An output stream to which to dump the data
+     * @param e the xexpression
+     */
+    template <typename E>
+    inline void dump_bin(std::ostream& stream, const xexpression<E>& e)
+    {
+        detail::dump_bin_stream(stream, e);
+    }
+
+    /**
+     * Save xexpression to BIN format
+     *
+     * @param filename The filename or path to dump the data
+     * @param e the xexpression
+     */
+    template <typename E>
+    inline void dump_bin(const std::string& filename, const xexpression<E>& e)
+    {
+        std::ofstream stream(filename, std::ofstream::binary);
+        if (!stream.is_open())
+        {
+            std::runtime_error("IO Error: failed to open file");
+        }
+        detail::dump_bin_stream(stream, e);
+    }
+
+    /**
+     * Save xexpression to BIN format in a string
+     *
+     * @param e the xexpression
+     */
+    template <typename E>
+    inline std::string dump_bin(const xexpression<E>& e)
+    {
+        std::stringstream stream;
+        detail::dump_bin_stream(stream, e);
+        return stream.str();
+    }
+
+    /**
+     * Loads a BIN file
+     *
+     * @param stream An input stream from which to load the file
+     * @tparam T select the type of the BIN file
+     * @tparam L select layout_type::column_major if you stored data in
+     *           Fortran format
+     * @return xarray with contents from BIN file
+     */
+    template <typename T, layout_type L = layout_type::dynamic>
+    inline auto load_bin(std::istream& stream)
+    {
+        std::vector<T> uncompressed_buffer = detail::load_bin_file<T>(stream);
+        std::vector<std::size_t> shape = {uncompressed_buffer.size()};
+        auto array = adapt(std::move(uncompressed_buffer), shape);
+        return array;
+    }
+
+    /**
+     * Loads a BIN file
+     *
+     * @param filename The filename or path to the file
+     * @tparam T select the type of the BIN file
+     * @tparam L select layout_type::column_major if you stored data in
+     *           Fortran format
+     * @return xarray with contents from BIN file
+     */
+    template <typename T, layout_type L = layout_type::dynamic>
+    inline auto load_bin(const std::string& filename)
+    {
+        std::ifstream stream(filename, std::ifstream::binary);
+        if (!stream.is_open())
+        {
+            std::runtime_error("load_bin: failed to open file " + filename);
+        }
+        return load_bin<T, L>(stream);
+    }
+
+    struct xbin_config
+    {
+    };
+
+    template <class E>
+    void load_file(std::istream& stream, xexpression<E>& e, const xbin_config&)
+    {
+        e.derived_cast() = load_bin<typename E::value_type>(stream);
+    }
+
+    template <class E>
+    void dump_file(std::ostream& stream, const xexpression<E> &e, const xbin_config& config)
+    {
+        dump_bin(stream, e);
+    }
+}  // namespace xt
+
+#endif
---
+++ test/test_xbin.cpp
@@ -0,0 +1,33 @@
+/***************************************************************************
+* Copyright (c) Johan Mabille, Sylvain Corlay and Wolf Vollprecht          *
+* Copyright (c) QuantStack                                                 *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#include "gtest/gtest.h"
+
+#include "xtensor/xbin.hpp"
+
+namespace xt
+{
+    TEST(xbin, dump_load)
+    {
+        xtensor<double, 2> data
+            {{ 1.0,  2.0,  3.0,  4.0},
+             {10.0, 12.0, 15.0, 18.0}};
+
+        const char* fname = "data.bin";
+        std::ofstream out_file(fname, std::ofstream::binary);
+        dump_file(out_file, data, xbin_config());
+
+        xarray<double> a;
+        std::ifstream in_file(fname, std::ifstream::binary);
+        load_file(in_file, a, xbin_config());
+        a.reshape({2, 4});
+
+        ASSERT_TRUE(all(equal(a, data)));
+    }
+}
