d5823f46cc36584b97879b16344489974ce94cd2
DerThorsten <tb@quality-match.com>
2021-06-03 11:31:40 +0200

    Initial draft for a specialized computed_assign for xfixed     * added overload for fixed/nonfixed shape via enable_if     * added tests

--- test/test_xassign.cpp
+++ test/test_xassign.cpp
@@ -10,6 +10,7 @@
 #include "gtest/gtest.h"
 #include "xtensor/xarray.hpp"
 #include "xtensor/xtensor.hpp"
+#include "xtensor/xfixed.hpp"
 
 #include "xtensor/xassign.hpp"
 #include "xtensor/xnoalias.hpp"
@@ -143,6 +144,98 @@ namespace xt
             EXPECT_EQ(a.shape(0), 2);
             EXPECT_EQ(a.shape(1), 3);
         }
+    }
 
+    TEST(xassign, fixed_shape)
+    {
+        // matching shape 1D
+        {
+            xt::xtensor_fixed<int, xt::xshape<2>> a = {2,3};
+            xt::xtensor_fixed<int, xt::xshape<2>> b = {3,4};
+
+            xt::noalias(a) += b;
+
+            EXPECT_EQ(a(0), 5);
+            EXPECT_EQ(a(1), 7);
+        }
+        //matching shape 2D
+        {
+            xt::xtensor_fixed<int, xt::xshape<2,2>> aa = {{1,2},{3,4}};
+            xt::xtensor_fixed<int, xt::xshape<2,2>> a(aa);
+            xt::xtensor_fixed<int, xt::xshape<2,2>> b = {{3,4},{5,6}};
+            xt::noalias(a) += b;
+
+            EXPECT_EQ(a(0,0),  aa(0,0) + b(0,0));
+            EXPECT_EQ(a(0,1),  aa(0,1) + b(0,1));
+            EXPECT_EQ(a(1,0),  aa(1,0) + b(1,0));
+            EXPECT_EQ(a(1,1),  aa(1,1) + b(1,1));
+        }
+        // b is broadcasted with matching dimension (first axis is singleton)
+        {
+            xt::xtensor_fixed<int, xt::xshape<2,2>> aa = {{1,2},{3,4}};
+            xt::xtensor_fixed<int, xt::xshape<2,2>> a(aa);
+            xt::xtensor_fixed<int, xt::xshape<2,1>> b = {{5,6}};
+            EXPECT_EQ(b.shape(0),2);
+            EXPECT_EQ(b.shape(1),1);
+            xt::noalias(a) += b;
+
+            EXPECT_EQ(a(0,0),  aa(0,0) + b.at(0,0));
+            EXPECT_EQ(a(0,1),  aa(0,1) + b.at(0,0));
+            EXPECT_EQ(a(1,0),  aa(1,0) + b.at(1,0));
+            EXPECT_EQ(a(1,1),  aa(1,1) + b.at(1,0));
+        }
+        // b is broadcasted with matching dimension (second axis is singleton)
+        {
+            xt::xtensor_fixed<int, xt::xshape<2,2>> aa = {{1,2},{3,4}};
+            xt::xtensor_fixed<int, xt::xshape<2,2>> a(aa);
+            xt::xtensor_fixed<int, xt::xshape<1,2>> b = {{3,4}};
+            EXPECT_EQ(b.shape(0),1);
+            EXPECT_EQ(b.shape(1),2);
+            xt::noalias(a) += b;
+
+            EXPECT_EQ(a(0,0),  aa(0,0) + b(0,0));
+            EXPECT_EQ(a(0,1),  aa(0,1) + b(0,1));
+            EXPECT_EQ(a(1,0),  aa(1,0) + b(0,0));
+            EXPECT_EQ(a(1,1),  aa(1,1) + b(0,1));
+        }
+        // broadcast with non matching dimensions
+        {
+            xt::xtensor_fixed<int, xt::xshape<2,2>> aa = {{1,2},{3,4}};
+            xt::xtensor_fixed<int, xt::xshape<2,2>> a(aa);
+            xt::xtensor_fixed<int, xt::xshape<2>> b = {3,4};
+
+            xt::noalias(a) += b;
+
+            EXPECT_EQ(a(0,0),  aa(0,0) + b(0));
+            EXPECT_EQ(a(0,1),  aa(0,1) + b(1));
+            EXPECT_EQ(a(1,0),  aa(1,0) + b(0));
+            EXPECT_EQ(a(1,1),  aa(1,1) + b(1));
+        }
+    }
+    TEST(xassign, fixed_raises)
+    {
+        // cannot broadcast a  itself on assignment
+        {
+            xt::xtensor_fixed<int, xt::xshape<2>> a = {2,3};
+            xt::xtensor_fixed<int, xt::xshape<2,2>> b = {{3,4},{3,4}};
+
+            EXPECT_THROW(xt::noalias(a) += b, xt::broadcast_error);
+        }
+
+        // cannot broadcast a  itself on assignment
+        {
+            xt::xtensor_fixed<int, xt::xshape<1,2>> a = {{3,4}};
+            xt::xtensor_fixed<int, xt::xshape<2,2>> b = {{3,4},{3,4}};
+
+            EXPECT_THROW(xt::noalias(a) += b, xt::broadcast_error);
+        }
+
+        // cannot broadcast a  itself on assignment
+        {
+            xt::xtensor_fixed<int, xt::xshape<2,1>> a = {{3},{4}};
+            xt::xtensor_fixed<int, xt::xshape<2,2>> b = {{3,4},{3,4}};
+
+            EXPECT_THROW(xt::noalias(a) += b, xt::broadcast_error);
+        }
     }
 }
