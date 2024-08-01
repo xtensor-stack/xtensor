9c68fbdc070f08b78333ae8310f64d01e3fd88be
André Offringa <offringa@astron.nl>
2024-08-01 16:24:45 +0200

    Fix various compiler warnings
    These solve warnings when compiled with gcc 14 using a set of warnings enabled that we use in one of our projects:
      -Wall
      -Wnon-virtual-dtor
      -Wzero-as-null-pointer-constant
      -Wduplicated-branches
      -Wundef
      -Wvla
      -Wpointer-arith
      -Wextra
      -Wno-unused-parameter

--- include/xtensor/xassign.hpp
+++ include/xtensor/xassign.hpp
@@ -456,16 +456,21 @@ namespace xt
         constexpr bool simd_strided_assign = traits::simd_strided_assign();
         if (linear_assign)
         {
-            if (simd_linear_assign || traits::simd_linear_assign(de1, de2))
-            {
-                // Do not use linear_assigner<true> here since it will make the compiler
-                // instantiate this branch even if the runtime condition is false, resulting
-                // in compilation error for expressions that do not provide a SIMD interface.
-                // simd_assign is true if simd_linear_assign() or simd_linear_assign(de1, de2)
-                // is true.
-                linear_assigner<simd_assign>::run(de1, de2);
-            }
-            else
+            // Do not use linear_assigner<true> here since it will make the compiler
+            // instantiate this branch even if the runtime condition is false, resulting
+            // in compilation error for expressions that do not provide a SIMD interface.
+            // simd_assign is true if simd_linear_assign() or simd_linear_assign(de1, de2)
+            // is true.
+            if constexpr(simd_assign) {
+                if(simd_linear_assign || traits::simd_linear_assign(de1, de2))
+                {
+                    linear_assigner<true>::run(de1, de2);
+                }
+                else
+                {
+                    linear_assigner<false>::run(de1, de2);
+                }
+            } else
             {
                 linear_assigner<false>::run(de1, de2);
             }
