a8ec29a34371ef1ee19ed38dc523650f90161170
André Offringa <offringa@gmail.com>
2024-11-22 08:26:04 +0100

    Undo whitespace changes

--- include/xtensor/xassign.hpp
+++ include/xtensor/xassign.hpp
@@ -458,19 +458,19 @@ namespace xt
         {
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wduplicated-branches"
-            if(simd_linear_assign || traits::simd_linear_assign(de1, de2))
+            if (simd_linear_assign || traits::simd_linear_assign(de1, de2))
             {
-            // Do not use linear_assigner<true> here since it will make the compiler
-            // instantiate this branch even if the runtime condition is false, resulting
-            // in compilation error for expressions that do not provide a SIMD interface.
-            // simd_assign is true if simd_linear_assign() or simd_linear_assign(de1, de2)
-            // is true.
+                // Do not use linear_assigner<true> here since it will make the compiler
+                // instantiate this branch even if the runtime condition is false, resulting
+                // in compilation error for expressions that do not provide a SIMD interface.
+                // simd_assign is true if simd_linear_assign() or simd_linear_assign(de1, de2)
+                // is true.
                 linear_assigner<simd_assign>::run(de1, de2);
-                }
-                else
-                {
-                    linear_assigner<false>::run(de1, de2);
-                }
+            }
+            else
+            {
+                linear_assigner<false>::run(de1, de2);
+            }
 #pragma GCC diagnostic pop
         }
         else if (simd_strided_assign)
