7910a2d2254d2b4552b7523684935a8679c2ede9
sbstndb/sbstndbs <30315405+sbstndb@users.noreply.github.com>
2025-04-09 12:37:25 +0200

    feat: Use cpp20 by default (#2839)

--- test/test_xbuilder.cpp
+++ test/test_xbuilder.cpp
@@ -676,7 +676,13 @@ namespace xt
 
         xt::dynamic_shape<std::size_t> sd = {3, 2, 1};
         auto ed1 = empty<double>(sd);
-        auto ed2 = empty<double, layout_type::column_major>(dynamic_shape<std::size_t>({3, 3, 3}));
+// TODO : The ed2 expression do not work on MSVC due to bad deduction since cpp20. We need to fix it for MSVC.
+#if defined(_MSVC_LANG)
+        using ShapeType = xt::dynamic_shape<std::size_t>;
+        auto ed2 = empty<double, xt::layout_type::column_major, ShapeType>(ShapeType({3, 3, 3}));
+#else
+        auto ed2 = empty<double, xt::layout_type::column_major>(xt::dynamic_shape<std::size_t>({3, 3, 3}));
+#endif
         auto ed3 = empty<double>(std::vector<std::size_t>({3, 3, 3}));
         b = std::is_same<decltype(ed1), xarray<double>>::value;
         EXPECT_TRUE(b);
